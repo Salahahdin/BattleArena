88f6842b4cb38ce14ffae0f0f9a1d478dfc1eec8
Salahahdin <alexbiedka@gmail.com>
2026-05-18 23:55:47 +0200

    Add initial project structure with .gitignore, XML configurations, and Enemy class

---
+++ src/main/java/battlearena/Enemy.java
@@ -0,0 +1,49 @@
+package battlearena;
+
+public class Enemy {
+
+    private String typeOfEnemy;
+    private int healthPoints = 10;
+    private int attackDamage = 1;
+
+    public Enemy(String typeOfEnemy, int healthPoints, int attackDamage){
+        this.typeOfEnemy = typeOfEnemy;
+        this.healthPoints = healthPoints;
+        this.attackDamage = attackDamage;
+    }
+
+    public String getTypeOfEnemy(){
+        return typeOfEnemy;
+    }
+
+    public void setTypeOfEnemy(String typeOfEnemy){
+        this.typeOfEnemy = typeOfEnemy;
+    }
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getAttackDamage(){
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public void talk(){
+        System.out.println("I am " + typeOfEnemy + "be prepared to fight!");
+    }
+
+    public void walkForward(){
+        System.out.println(typeOfEnemy + " moves closer to you");
+    }
+
+    public void attack(){
+        System.out.println(typeOfEnemy + " attacks for " + attackDamage + " damage");
+    }
+}
---
+++ src/main/java/battlearena/Main.java
@@ -0,0 +1,19 @@
+package battlearena;
+
+public class Main {
+    public static void main(String[] args) {
+        Enemy zombie = new Enemy("Zombie", 10, 1);
+        Enemy ogre = new Enemy("Ogre", 20,3 );
+
+        System.out.println(zombie.getTypeOfEnemy() + " has " + zombie.getHealthPoints() + " health points and can do attack of " + zombie.getAttackDamage());
+
+        zombie.talk();
+        zombie.walkForward();
+        zombie.attack();
+
+
+    }
+
+
+
+}
