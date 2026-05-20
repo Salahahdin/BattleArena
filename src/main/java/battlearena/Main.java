4cc2c5ca599ccb110b6876c36de45a2723d8492b
Salahahdin <alexbiedka@gmail.com>
2026-05-20 14:28:45 +0200

    Refactor Enemy class to include unique IDs and track total enemies; update Main to implement battle method

--- src/main/java/battlearena/Main.java
+++ src/main/java/battlearena/Main.java
@@ -5,11 +5,14 @@ public class Main {
         Zombie zombie = new Zombie(10, 1);
         Ogre ogre = new Ogre( 20,3 );
 
-        System.out.println("Enemy has " + zombie.getHealthPoints() + " health points and can do attack of " + zombie.getAttackDamage());
+        battle(zombie);
+        battle(ogre);
 
-        zombie.talk();
-        zombie.spreadDisease();
-        ogre.talk();
+
+    }
+    public static void battle(Enemy e){
+        e.talk();
+        e.attack();
     }
 
 
