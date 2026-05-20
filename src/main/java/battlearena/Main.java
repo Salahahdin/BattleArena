4b8f24bafe308872d313ac6003e7b3b4584001b1
Salahahdin <alexbiedka@gmail.com>
2026-05-20 22:05:59 +0200

    Introduce Hero and Weapon classes; refactor Enemy class to be abstract and update battle logic in Main

--- src/main/java/battlearena/Main.java
+++ src/main/java/battlearena/Main.java
@@ -11,7 +11,14 @@ public class Main {
 
         zombie.battleStance();
         ogre.stareDown();
-        battle(zombie, ogre);
+
+        //battle(zombie, ogre);
+
+        Weapon weapon = new Weapon("Sword", 5);
+        Hero hero = new Hero(25,1);
+        hero.setWeapon(weapon);
+        hero.equipWeapon();
+        heroBattle(hero,ogre);
     }
     public static void battle(Enemy e1, Enemy e2){
         e1.talk();
@@ -38,6 +45,24 @@ public class Main {
 
     }
 
+    public static void heroBattle(Hero hero, Enemy enemy){
+        while(hero.getHealthPointsRemaining() >0 && enemy.getHealthPointsRemaining() >0){
+            System.out.println("------");
+            enemy.specialAttack();
+            System.out.println("Hero: " + hero.getHealthPointsRemaining() + " HP left");
+            System.out.println("Enemy: " + enemy.getHealthPointsRemaining() + " HP left");
+            enemy.attack();
+            hero.setHealthPointsRemaining(hero.getHealthPointsRemaining()- enemy.getAttackDamage());
+            hero.attack();
+            enemy.setHealthPointsRemaining(enemy.getHealthPointsRemaining()- hero.getAttackDamage());
 
+        }
+        System.out.println("------");
+        if (hero.getHealthPointsRemaining()>0){
+            System.out.println("Hero wins!");
+        } else{
+            System.out.println("Enemy wins!");
+        }
+    }
 
 }
